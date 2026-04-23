6648442272d95aecad0ba50eabee6554cda33a77
Sriram P C <sriram.pc@gmail.com>
2026-04-23 18:37:36 -0400

    update script to run three times

--- runs/pbmc64.sh
+++ runs/pbmc64.sh
@@ -3,3 +3,5 @@ mkdir -p output/
 CMD="julia -p 64 scripts/runPIDCParallel.jl -e . -r 4 --hdf5 ./data/pbmc/adata.20K.5K.h5ad ./output/pidc_64p.csv"
 echo $CMD
 $CMD 2>&1 | tee output/pidc.log
+$CMD 2>&1 | tee -a output/pidc.log
+$CMD 2>&1 | tee -a output/pidc.log
